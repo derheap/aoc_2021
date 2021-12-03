c83770d3848c7b2bef0d19e0f420066524bd7bdd
Tobias <tobias@hiep.de>
2021-12-03 09:16:12 +0100

    Solve day 2.

--- 02.exs
+++ 02.exs
@@ -1,4 +1,3 @@
-
 import ExUnit.Assertions
 
 test_input = """
@@ -11,35 +10,58 @@ forward 2
 """
 
 defmodule One do
-
   defp parse(text) do
     text
     |> String.split("\n")
     |> Enum.map(&String.trim/1)
-    |> Enum.filter(fn it -> String.length(it) > 0 end)
-    |> Enum.map(fn it -> String.split(it, " ") end)
-    |> Enum.map(fn it -> {Enum.at(it,0), String.to_integer(Enum.at(it,1))} end)
+    |> Enum.filter(&(String.length(&1) > 0))
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.map(&{Enum.at(&1, 0), String.to_integer(Enum.at(&1, 1))})
+  end
+
+  defp dive({move, change}, {pos, depth}) do
+    case {move, change} do
+      {"forward", change} -> {pos + change, depth}
+      {"up", change} -> {pos, depth - change}
+      {"down", change} -> {pos, depth + change}
+      _ -> {pos, depth}
+    end
+  end
+
+  defp dive_aim({move, change}, {pos, depth, aim}) do
+    case {move, change} do
+      {"forward", change} -> {pos + change, depth + aim * change, aim}
+      {"up", change} -> {pos, depth, aim - change}
+      {"down", change} -> {pos, depth, aim + change}
+      _ -> {pos, depth}
+    end
   end
 
   def part_one(input) do
     input
     |> parse()
+    |> Enum.reduce({0, 0}, &dive/2)
+    |> then(fn {pos, depth} -> pos * depth end)
   end
 
   def part_two(input) do
     input
     |> parse()
+    |> Enum.reduce({0, 0, 0}, &dive_aim/2)
+    |> then(fn {pos, depth, _} -> pos * depth end)
   end
-
-
 end
 
 test_input
 |> One.part_one()
 |> IO.inspect()
 
-#assert One.part_one(test_input) == 7
-#assert One.part_two(test_input) == 5
+test_input
+|> One.part_two()
+|> IO.inspect()
+
+assert One.part_one(test_input) == 150
+assert One.part_two(test_input) == 900
 
 input = File.read!("input/02.txt")
 
@@ -47,6 +69,6 @@ input
 |> One.part_one()
 |> IO.inspect()
 
-#input
-#|> One.part_two()
-#|> IO.inspect()
+input
+|> One.part_two()
+|> IO.inspect()
